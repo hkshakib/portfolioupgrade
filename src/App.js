e62a59226a9f45e3f1d405518cf3cc73f04bf0da
hkshakib <humayunkibriashakib@gmail.com>
2023-08-07 15:20:09 +0600

    feat: Added Layout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import { Grid, GridItem } from "@chakra-ui/react";
+
 function App() {
-  return <div className="App">Portfolio</div>;
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+      }}
+      templateColumns={{
+        base: "1fr",
+        lg: "1fr",
+      }}
+    >
+      <GridItem area="nav" bg="blue">
+        Navbar
+      </GridItem>
+      <GridItem area="main" bg="green">
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
